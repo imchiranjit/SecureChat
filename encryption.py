569662c52c3e6f789b3c22989e16c5577eef075e
Chiranjit <cmandal2003@gmail.com>
2022-09-04 14:44:45 +0530

    Used cryptography module

--- encryption.py
+++ encryption.py
@@ -1,7 +1,11 @@
-#Importing RSA module
-import rsa
-#Importig pickle module for sending objects through the socket
-import pickle
+#Importing base64 for encoding and decoding of base64
+import base64
+#Importing all RSA modules from the cryptography
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
 
 #Encryption Class
 class Encryption:
@@ -9,7 +13,14 @@ class Encryption:
     def __init__(self):
         #setting class attributes
         #Generating new Keys
-        self.setKeys(rsa.newkeys(512))
+        private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=4096,
+            backend=default_backend()
+        )
+        public_key = private_key.public_key()
+        #Setting keys
+        self.setKeys(public_key, private_key)
     
     #method to set private key or key for the decryption
     def setDecryptionKey(self, decryptionKey):
@@ -19,41 +30,70 @@ class Encryption:
     def setEncryptionKey(self, encryptionKey):
         self.encryptionKey = encryptionKey
 
+    #method to set public key or key for the encryption of this object
+    def setMyEncryptionKey(self, encryptionKey):
+        self.myEncryptionKey = encryptionKey
+
     #Set Both Encryption and Decryption keys simultaneously
-    def setKeys(self, keys):
-        self.setEncryptionKey(keys[0])
-        self.setDecryptionKey(keys[1])
+    def setKeys(self, public_key, private_key):
+        self.setMyEncryptionKey(public_key)
+        self.setEncryptionKey(public_key)
+        self.setDecryptionKey(private_key)
 
     #method to get public key or key for the encryption
     def getEncryptionKey(self):
         return self.encryptionKey
+
+    #method to get public key or key for the encryption of this object
+    def getMyEncryptionKey(self):
+        return self.myEncryptionKey
 
     #method to get private key or key for the decryption
     def getDecryptionKey(self):
         return self.decryptionKey
 
     #method to set pickled public key or key for the encryption
-    def setPickledEncryptionKey(self, encryptionKey):
-        #Unpicklling the encryption key
-        unpickledEncryptionKey = pickle.loads(encryptionKey)
-        self.setEncryptionKey(unpickledEncryptionKey)
+    def setSerializedEncryptionKey(self, encryptionKey):
+        #Unserialization the encryption key
+        unserializedEncryptionKey = serialization.load_pem_public_key(
+            encryptionKey,
+            backend=default_backend()
+        )
+        self.setEncryptionKey(unserializedEncryptionKey)
 
     #method to get pickled public key or key for the encryption
-    def getPickledEncryptionKey(self):
-        #Picklling
-        pickledEncryptionKey = pickle.dumps(self.getEncryptionKey())
-        return pickledEncryptionKey
+    def getSerializedEncryptionKey(self):
+        #Serialization
+        serializedEncryptionKey = self.encryptionKey.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+        return serializedEncryptionKey
 
     #Method to encrypt the text with public key or encryption key
     def encrypt(self, text):
         try:
-            return rsa.encrypt(text.encode(), self.encryptionKey)
+            return base64.b64encode(self.encryptionKey.encrypt(
+                text.encode(),
+                padding.OAEP(
+                    mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                    algorithm=hashes.SHA256(),
+                    label=None
+                )
+            ))
         except Exception as err:
             return text.encode()
 
     #Method to decrypt the encrypted text with private key or decryption key
     def decrypt(self, encText):
         try:
-            return rsa.decrypt(encText, self.decryptionKey).decode()
+            return self.decryptionKey.decrypt(
+                base64.b64decode(encText),
+                padding.OAEP(
+                    mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                    algorithm=hashes.SHA256(),
+                    label=None
+                )
+            ).decode()
         except:
             return encText
